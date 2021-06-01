620b8d04e5170e51c1a6af3338084f54d8d42469
HmacSHA256 <DiracDelta@tutanota.com>
2021-06-01 15:17:43 -0400

    Initialized BlueprintJS POC.

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import { render } from "react-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+import "normalize.css";
+import "@blueprintjs/core/lib/css/blueprint.css";
+import "@blueprintjs/icons/lib/css/blueprint-icons.css";
+import "@blueprintjs/datetime/lib/css/blueprint-datetime.css";
+import "@blueprintjs/popover2/lib/css/blueprint-popover2.css";
+import "@blueprintjs/select/lib/css/blueprint-select.css";
+import "./app.css";
+
+import { Navigation } from "./Navigation";
+import { CoreExample } from "./CoreExample";
+import { SelectExample } from "./SelectExample";
+import { DatetimeExample } from "./DatetimeExample";
+
+const App = () => (
+  <div>
+    <Navigation />
+    <CoreExample />
+    <SelectExample />
+    <DatetimeExample />
+  </div>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+render(<App />, document.getElementById("root"));
